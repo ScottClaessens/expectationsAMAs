997ca16dc95335e38ff08351695a73cef41bceb4
Scott Claessens <scott.claessens@gmail.com>
2025-12-19 13:03:33 +0000

    Re-run pilot pipeline

--- _targets.R
+++ _targets.R
@@ -14,9 +14,9 @@ tar_source()
 # targets pipeline
 list(
   # main pipelines
-  #targets_pilot1,   # R/pilot1/targets_pilot1.R
-  #targets_pilot2,   # R/pilot2/targets_pilot2.R
-  #targets_pilot3,   # R/pilot3/targets_pilot3.R
+  targets_pilot1,   # R/pilot1/targets_pilot1.R
+  targets_pilot2,   # R/pilot2/targets_pilot2.R
+  targets_pilot3,   # R/pilot3/targets_pilot3.R
   targets_study1,   # R/study1/targets_study1.R
   targets_study2,   # R/study2/targets_study2.R
   targets_study3    # R/study3/targets_study3.R
